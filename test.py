7c9d81daa53af6ec239fb1b345bd6c52c1f37e45
rakeshraih <rakeshraih@gmail.com>
2024-02-09 19:37:46 -0800

    Add all

---
+++ test.py
@@ -0,0 +1,35 @@
+# import json
+# path = "./label-studio/annotations/result.json"
+# f = open(path)
+# anns = json.load(f)
+# print(anns.keys())
+from pycocotools.coco import COCO
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+ann_file = "./label-studio/annotations/result.json"
+coco=COCO(ann_file)
+ 
+# Get list of category_ids, here [2] for bicycle
+category_ids = coco.getCatIds(['grated'])
+
+
+# Get list of image_ids which contain bicycles
+image_ids = coco.getImgIds(catIds=[2])
+print(image_ids[0:5])
+
+image = Image.open("./IMG_5661.jpg")
+
+annotation_ids = coco.getAnnIds(imgIds=0, catIds=[2])
+anns = coco.loadAnns(annotation_ids)
+
+
+fig, ax = plt.subplots()
+for ann in anns:
+    box = ann['bbox']
+    bb = patches.Rectangle((box[0],box[1]), box[2],box[3], linewidth=2, edgecolor="blue", facecolor="none")
+    ax.add_patch(bb)
+ 
+ax.imshow(image)
+plt.show()
